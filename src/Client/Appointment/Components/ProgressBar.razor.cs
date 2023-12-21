5d229843c2d31ea61d476df36a590ed968c4ea0c
Yentl Demeulemeester <yentl.demeulemeester@student.hogent.be>
2023-12-21 19:57:18 +0100

    test

--- src/Client/Appointment/Components/ProgressBar.razor.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.AspNetCore.Components;
-
-namespace Client.Appointment.Components { 
-	public partial class ProgressBar {
-		[Parameter] public int Active { get; set; } = default;
-	}
-}
--- src/Shared/Users/Teams/Groups/IGroupService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shared.Users.Teams.Groups;
-
-public interface IGroupService
-{
-	Task<GroupResult.Index> GetIndexAsync(GroupRequest.Index request);
-	Task<long> CreateAsync(GroupDto.Mutate model);
-	Task DeleteAsync(long groupId);
-	Task EditAsync(long groupId, GroupDto.Mutate model);
-}
